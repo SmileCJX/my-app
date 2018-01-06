f593486c21f26825baed060c1632e5d1a594c670
SmileCJX <1092925421@qq.com>
2018-01-06 18:05:04 +0800

    商品评论，动态评分，评论列表，评论框隐藏

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -13,6 +13,12 @@ export class ProductDetailComponent implements OnInit {
 
   comments : Comment[];
 
+  newRating: number = 5;
+
+  newComment: string = '';
+
+  isCommentHide: boolean = true;
+
   constructor(private routeInfo : ActivatedRoute,
                 private productService : ProductService) { }
 
@@ -20,6 +26,18 @@ export class ProductDetailComponent implements OnInit {
     let productId : number = this.routeInfo.snapshot.params['productId'];
     this.products = this.productService.getProduct(productId);
     this.comments = this.productService.getCommentsForProductId(productId);
+  }
+
+  addComment() {
+    let comment = new Comment(0,this.products[0].id,new Date().toISOString(),"someone",this.newRating,this.newComment);
+    this.comments.unshift(comment);
+
+    let sum = this.comments.reduce((sum, comment) => sum + comment.rating, 0);
+    this.products[0].rating = sum / this.comments.length;
+
+    this.newComment  = null;
+    this.newRating = 5;
+    this.isCommentHide = true;
   }
 
 }
